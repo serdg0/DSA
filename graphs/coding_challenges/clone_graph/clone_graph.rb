878cd9cc5c251589b7bd055fcd349e95732bf9c8
serdg0 <sergio.diazg92@gmail.com>
2020-05-22 20:13:54 -0500

    Detected bug

--- graphs/coding_challenges/clone_graph/clone_graph.rb
+++ graphs/coding_challenges/clone_graph/clone_graph.rb
@@ -1,23 +1,28 @@
 def node_array(node)
-    sub = []
-  
-    node.neighbors.each do |n|
-      sub.push(n.val)
-    end
-    
-    sub
+  sub = []
+
+  node.neighbors.each do |n|
+    sub.push(n.val)
   end
   
-  def cloneGraph(node)
-    clone = []
-    visiteds = []
-    
-    while !visiteds.include?(node.val) do
-      visiteds.push(node.val) #mark as visited
-      neighs = node_array(node) #check their neighbors
-      clone[node.val - 1] = neighs #add to clone
-       #keep traversing
-      node = 
-    end
-    clone
+  sub
 end
+
+def cloneGraph(node)
+  clone = []
+  visiteds = []
+  
+  while !visiteds.include?(node.val) do
+    break if node.val.nil?
+    visiteds.push(node.val) #mark as visited
+    neighs = node_array(node) #check their neighbors
+    clone[node.val - 1] = neighs #add to clone
+    to_visit = node.neighbors.select { |x| !visiteds.include?(x.val)}
+    node = to_visit[0]
+    p node.val
+  end
+  clone
+end
+
+# THE ERROR IS THAT IN THE LAST ITERATION, WHEN THERE ARE NO MORE VISITEDS, node.val
+# AS A CONDITION IS NIL. FIX THE BUG AND OPTIMIZE
