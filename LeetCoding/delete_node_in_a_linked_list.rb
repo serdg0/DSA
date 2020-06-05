d4bae55f488dda970118bc963784f05219f7befb
serdg0 <sergio.diazg92@gmail.com>
2020-06-05 16:07:11 -0500

    delete node in a linked list

---
+++ LeetCoding/delete_node_in_a_linked_list.rb
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+# class ListNode
+#     attr_accessor :val, :next
+#     def initialize(val)
+#         @val = val
+#         @next = nil
+#     end
+# end
+
+# @param {ListNode} node
+# @return {Void} Do not return anything, modify node in-place instead.
+def delete_node(node)
+    node.val = node.next.val
+    node.next = node.next.next
+end
+
+# Runtime: 32 ms
+# Memory Usage: 9.4 MB
+# https://leetcode.com/problems/delete-node-in-a-linked-list/
