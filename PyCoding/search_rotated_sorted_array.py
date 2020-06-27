0d72e8c5254611829129d13fbdbcccee0c7cc4c9
serdg0 <sergio.diazg92@gmail.com>
2020-06-27 16:15:31 -0500

    search in rotated array - wuuuut!

--- PyCoding/search_rotated_sorted_array.py
+++ PyCoding/search_rotated_sorted_array.py
@@ -1,14 +1,19 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
+      if len(nums) is 0:
+        return -1
       
       def get_pivot(arr):
+        if len(arr) is 1:
+          return 0
+        
         for i in range(len(arr) - 1):
           if arr[i] > arr[i+1]:
             return i
         return -1
       
       def binary_search(left, right, arr, target):
-    
+        
         while left <= right:
           pivot = (left + right) // 2
         
@@ -22,16 +27,15 @@ class Solution:
         return -1
     
       pivot = get_pivot(nums)
-      
-      if nums[pivot] is target:
-        return pivot
-        
       left, right = 0, len(nums) - 1
       
       if pivot is -1:
         pivot = (left + right) // 2
+      
+      if nums[pivot] == target:
+        return pivot
       print(pivot)
-      if target < nums[pivot]:
+      if target < nums[0]:
         return binary_search(pivot + 1, right, nums, target)
       else:
-        return binary_search(pivot + 1, right, nums, target)
+        return binary_search(left, pivot - 1 , nums, target)
