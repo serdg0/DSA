ccae135c8a8f1c32cc11cf5c244ee8b647c2b29b
Sergio Diaz <sergio.diazg92@gmail.com>
2020-07-06 16:50:57 -0500

    finish first bad version

---
+++ LeetCoding/first_bad_version.rb
@@ -0,0 +1,20 @@
+def first_bad_version(n)
+    return 1 if is_bad_version(1)
+    
+    left, right = 1, n
+    
+    while left <= right do
+        piv = (left + right) / 2
+        current = is_bad_version(piv)
+        next_one = is_bad_version(piv + 1)
+
+        if !current && next_one
+            return piv + 1
+        elsif !current && !next_one
+            left = piv + 1
+        else
+            right = piv - 1
+        end
+    end
+    
+end
